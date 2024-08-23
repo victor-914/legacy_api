35eae215b69372443e67d2928960a81dbf4fffc2
victor <103121630+victor-914@users.noreply.github.com>
2024-08-23 16:18:46 +0100

    db_connection

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,52 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": [
+            "'self'",
+            "http:",
+            "https:",
+            "ec2-13-51-65-133.eu-north-1.compute.amazonaws.com",
+          ],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "res.cloudinary.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "res.cloudinary.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  {
+    name: "strapi::body",
+    config: {
+      jsonLimit: "200mb",
+      formLimit: "200mb",
+      textLimit: "200mb",
+      formidable: {
+        maxFileSize: 20 * 1024 * 1024,
+      },
+    },
+  },
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/server.js
+++ config/server.js
@@ -1,10 +1,24 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT"),
+  url: env("PUBLIC_URL"),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  settings: {
+    parser: {
+      enabled: true,
+      multipart: true,
+      formLimit: "900mb", // modify here limit of the form body
+      jsonLimit: "900mb", // modify here limit of the JSON body
+      textLimit: "900mb", // modify here limit of the text body
+
+      formidable: {
+        maxFileSize: 200 * 1024 * 1024, // Adjust the maxFileSize according to your needs
+      },
+    },
   },
 });
